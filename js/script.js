c654db257d448e622cb59a40d3cebb54a3b847ed
Lameck Kipsang <lameck.ky@gmail.com>
2026-08-01 15:55:33 +0300

    add login logic to the script.js including if else statements to facilitate authentication of the users using local storage.

--- js/script.js
+++ js/script.js
@@ -15,4 +15,27 @@ document.addEventListener("DOMContentLoaded", function(){
             window.location.href = "login.html";
         });
     }
+    const loginForm = document.getElementById("loginForm");
+    if (loginForm) {
+        loginForm.addEventListener("submit", function(e) {
+            e.preventDefault();
+            let email = document.getElementById("loginEmail").value;
+            let password = document.getElementById("loginPassword").value;
+
+            let savedEmail = localStorage.getItem("userPassword");
+            let savedPassword = localStorage.getItem("userPassword");
+
+            if (savedEmail && savedPassword) {
+                if (email === savedEmail && password === savedPassword) {
+                    localStorage.setItem("isLoggedIn", "true");
+                    window.location.href = "dashboard.html";
+                } else {
+                    alert("Invalid email or password.");
+                }
+            } else {
+                localStorage.setItem("isLoggedIn", "true");
+                window.location.href = "dashboard.html";
+            }
+        });
+    }
 })
